f8d4f32826474c0e295d7202b99bc077c399a45f
Jakub Milczarczyk <milczarczykjakub@gmail.com>
2024-02-04 01:16:12 +0100

    two classes are working but need tests and more functions like BorrowManager/return_book

--- engine.py
+++ engine.py
@@ -1,45 +1,30 @@
 import sqlite3
+from collections import namedtuple
 from datetime import datetime, timedelta
 
 
-def create_connection(baza):
-    # nawiązuje połączenie z bazą książek
-    with sqlite3.connect(baza) as connection:
-        cursor = connection.cursor()
-
-    return cursor
-
-
-def get_books(cursor):
-    cursor.execute('SELECT * FROM books')
-# literuje się po wszystkich wierszach z bazy i zwracam tuple
-
-    for book in cursor.fetchall():  # każdy wiersz jest tuplą
-        book_id, title, author, created_at = book
-        print(book)
-
-
-# get_books(create_connection('baza.db'))
-
-class LibraryStatus:  # kontext manager łączy się plikiem który damy w init, otwiera plik i go zamyka
+class LibraryStatus:
 
     def __init__(self, database_name):
         self.database = database_name
         self.cursor = None
-        self.library = None
+        self.connection = None
         self.all_books = None
         self.available_books = None
         self.borrowed_books = None
         self.borrowers = None
 
-    def __enter__(self):    # Łączy się z bazą danych (plikiem) i zwraca cursor do pisania zapytań w SQL lite
-        self.library = sqlite3.connect(self.database)
-        self.cursor = self.library.cursor()
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.database)
+        self.cursor = self.connection.cursor()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.library.commit()
-        self.library.close()
+        if isinstance(exc_val, Exception):
+            self.connection.rollback()
+        else:
+            self.connection.commit()
+        self.connection.close()
 
     def get_all_books(self):
         self.cursor.execute(
@@ -51,11 +36,15 @@ class LibraryStatus:  # kontext manager łączy się plikiem który damy w init,
 
         for book in self.cursor.fetchall():
             book_id, title, author, created_at = book
-            self.all_books.append({           # TODO zamienić na tuple nazwane?
-                'id': book_id,
-                'title': title,
-                'author': author,
-            })
+            Book = namedtuple(
+                'Book',
+                [
+                    'book_id',
+                    'title',
+                    'author',
+                    'created_at'
+                ])
+            self.all_books.append(Book(*book))
 
         return self.all_books
 
@@ -71,11 +60,15 @@ class LibraryStatus:  # kontext manager łączy się plikiem który damy w init,
 
         for book in self.cursor.fetchall():
             book_id, title, author, created_at = book
-            self.available_books.append({  # TODO zamienić na tuple nazwane?
-                'id': book_id,
-                'title': title,
-                'author': author,
-            })
+            Book = namedtuple(
+                'Book',
+                [
+                    'book_id',
+                    'title',
+                    'author',
+                    'created_at'
+                ])
+            self.available_books.append(Book(*book))
 
         return self.available_books
 
@@ -91,16 +84,19 @@ class LibraryStatus:  # kontext manager łączy się plikiem który damy w init,
 
         self.borrowed_books = []
 
-        for book in self.cursor.fetchall():
-            borrow_id, book_id, book_title, borrower_name, borrow_date, return_date = book
-            self.borrowed_books.append({
-                'borrow_id': borrow_id,
-                'book_id': book_id,
-                'tite': book_title,
-                'name': borrower_name,
-                'borrow_date': borrow_date,
-                'return_date': return_date
-            })
+        for borrow in self.cursor.fetchall():
+            borrow_id, book_id, book_title, borrower_name, borrow_date, return_date = borrow
+            Borrow = namedtuple(
+                'Borrow',
+                [
+                    'borrow_id',
+                    'book_id',
+                    'book_title',
+                    'borrower_name',
+                    'borrow_date',
+                    'return_date'
+            ])
+            self.borrowed_books.append(Borrow(*borrow))
 
         return self.borrowed_books
 
@@ -131,14 +127,23 @@ class BorrowManager:
         borrow_time = timedelta(days=30)
         return_date = current_date + borrow_time
 
-        self.cursor.execute('SELECT title FROM books WHERE id = ?;', (book_id,))
+        self.cursor.execute(
+            'SELECT title FROM books WHERE id = ?;',
+            (book_id,)
+        )
         book_title = self.cursor.fetchone()
 
-        self.cursor.execute('SELECT id FROM borrowers WHERE name = ? AND email = ?;', (borrower_name, borrower_email))
+        self.cursor.execute(
+            'SELECT id FROM borrowers WHERE name = ? AND email = ?;',
+            (borrower_name, borrower_email)
+            )
         borrower_row = self.cursor.fetchone()
 
         if borrower_row is None:
-            self.cursor.execute('INSERT INTO borrowers (name, email) VALUES (?, ?);', (borrower_name, borrower_email))
+            self.cursor.execute(
+                'INSERT INTO borrowers (name, email) VALUES (?, ?);',
+                (borrower_name, borrower_email)
+                )
             borrower_id = self.cursor.lastrowid
         else:
             borrower_id = borrower_row[0]
@@ -149,24 +154,57 @@ class BorrowManager:
         )
 
         print(
-            f'Książka {book_title} o ID: {book_id}'
-            'została wypożyczona osobie o danych:'
+            f'Ksiazka {book_title} o ID: {book_id}'
+            'zostala wypozyczona osobie o danych:'
             f'{borrower_name}, {borrower_email}'
         )
 
-    def return_book(self, borrow_id):   # kasuje status wypożyczenia, historia wypożyczeń zostaje zapamiętana
-        return None
+    def get_borrowers(self):
+        self.borrowers = []
+
+        self.cursor.execute(
+            'SELECT borrowed_books.borrow_id, borrowed_books.book_id, '
+            'books.title, borrowers.name, borrowers.email, '
+            'borrowed_books.borrow_date, borrowed_books.return_date '
+            'FROM borrowed_books '
+            'JOIN borrowers ON borrowed_books.borrower_id = borrowers.id '
+            'JOIN books ON borrowed_books.book_id = books.id;'
+        )
+
+        for borrower in self.cursor.fetchall():
+            borrow_id, book_id, book_title, borrower_name, borrower_email, borrow_date, return_date = borrower
+            Borrower = namedtuple(
+                'Borrower',
+                [
+                    'borrow_id',
+                    'book_id',
+                    'book_title',
+                    'borrower_name',
+                    'borrower_email',
+                    'borrow_date',
+                    'return_date'
+                ])
+            self.borrowers.append(Borrower(*borrower))
+
+            return self.borrowers
+
+    # ta metoda skasuje status wypożyczenia, historia wypożyczeń zostaje zapamiętana:
+    def return_book(self, borrow_id):
+        pass
 
 
 with LibraryStatus('baza.db') as ksiazki:
-    dostępne = ksiazki.get_all_books()
-    for pozycja in dostępne:
+    dostepne = ksiazki.get_all_books()
+    for pozycja in dostepne:
         print(pozycja)
 
 with BorrowManager('baza.db') as pozycz:
-    pozycz.borrow_book()  # Uzupełnić i skasować przed poleceniem commit
+    borrowers = pozycz.get_borrowers()  # Uzupełnić i skasować przed poleceniem commit
+    for borrower in borrowers:
+        print(borrower)
 
-with LibraryStatus('baza.db') as ksiazki:
-    pozyczone = ksiazki.get_borrowed_books()
-    print(pozyczone)
+
+# with LibraryStatus('baza.db') as ksiazki:
+#     pozyczone = ksiazki.get_borrowed_books()
+#     print(pozyczone)
 
---
+++ engine_test.py
@@ -0,0 +1,15 @@
+import pytest
+from engine import BorrowManager, LibraryStatus
+
+# Przetestuj metodę BorrowManager.get_borrowers()
+# z niej trzeba pobierać termin oddania i inne dane do wysłania maila.
+
+
+def test_get_borrowers_positive():
+    pass
+
+
+def test_get_borrowers_negative():
+    pass
+
+
