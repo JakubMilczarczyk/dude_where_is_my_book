f91a3eaa1e667b2a209e26a5f41426678568a5cc
Jakub Milczarczyk <milczarczykjakub@gmail.com>
2024-02-16 23:35:04 +0100

    "for" loop and SMTP server connection works better, does not work probably code in message is to complicated

--- sand_mail.py
+++ sand_mail.py
@@ -5,35 +5,6 @@ from collections import namedtuple
 from datetime import datetime
 
 # TODO import i uzycie biblioteki: https://jinja.palletsprojects.com/en/3.1.x/intro/
-
-# przykładowy kod do testowania mailingu w Mail Trap:
-# sender = "Private Person <from@example.com>"
-# receiver = "A Test User <to@example.com>"
-#
-# message = f"""\
-# Subject: Hi Mailtrap
-# To: {receiver}
-# From: {sender}
-#
-# This is a test e-mail message."""
-#
-# with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
-#     server.login("308f7c6fcd7f98", "********7428")   # <- Wstaw dane z Mail Trap
-#     server.sendmail(sender, receiver, message)
-#
-# #  Przykład Template - do przerobienia na wiadomości z podstawianymi danymi naszych dłużników:
-#
-# wiadomosc = Template('$borrower_name nie oddałeś mi książki $book_title a termin minął $return_date!')
-#
-# info = [
-#     ('Kuba', 'Zawód Programista', '2024-02-14'),
-#     ('Leon', 'Gimnastyka dla malucha', '2024-01-29')
-# ]
-
-# for borrower_name, book_title, return_date in info:
-#     text = wiadomosc.substitute(borrower_name=borrower_name, book_title=book_title, return_date=return_date)
-#     print(text)
-
 # Moj obiekt wysyłajacy maila:
 
 
@@ -87,12 +58,12 @@ class EmailSender:
         borrows = self.check_books_after_return_date()
 
         message_content = Template(f'''
-        Hej &borrower_name!
+        Hej $borrower_name!
         
-        Piszę do Ciebie bo jest już {datetime.today()} a umawialiśmy się,
-        że książka &book_title, którą Ci pożyczyłem &borrow_date wróci do mnie do &return_date.
+        Pisze do Ciebie bo jest juz {datetime.strftime(datetime.today(), format='%Y-%m-%d')} a umawialismy sie,
+        ze ksiazka $book_title, ktora Ci pozyczylem $borrow_date wroci do mnie do $return_date.
         
-        Proszę o zwrot książki.
+        Prosze o zwrot ksiazki.
         
         Kuba
         ''')
@@ -100,28 +71,30 @@ class EmailSender:
         # wstawiać w miejscu "sender" mojego maila
         # wstawiać w miejscu "reciver" borrow.borrower_email
 
-        for borrow in borrows:
-            mail = message_content.substitute(
-                borrower_name=borrow.borrower_name,
-                book_title=borrow.book_title,
-                borrow_date=borrow.borrow_date,
-                return_date=borrow.return_date
-            )
-            sender = "Private Person <from@example.com>"
-            receiver = "A Test User <to@example.com>"
+        with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
+            server.login("*******", "*********")  # Wstaw dane z Mail Trap
 
-            message = f"""\
-            Subject: Przypomnienie o zwrocie ksiazki
-            To: {receiver}
-            From: {sender}
-    
-            {mail}"""
+            for borrow in borrows:  # TODO aktuanie pomija znaki ale to lipa jest:
+                mail = message_content.substitute(
+                    borrower_name=str(borrow.borrower_name),
+                    book_title=str(borrow.book_title.encode('ascii', 'ignore').decode('ascii')),
+                    borrow_date=str(borrow.borrow_date),
+                    return_date=str(borrow.return_date)
+                )
 
-            with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
-                server.login("************", "*******57428")  # Wstaw dane z Mail Trap
-                message = message.encode('utf-8')
+                sender = "Private Person <from@example.com>"
+                receiver = "A Test User <to@example.com>"
+
+                message = f"""\
+                Subject: Przypomnienie o zwrocie ksiazki
+                To: {receiver}
+                From: {sender}
+        
+                 {mail}"""
+
                 server.sendmail(sender, receiver, message)
-                text = 'mail wyslany :)'
+
+                text = f'Mail o tresci:\n{message} \n Mail zostal wyslany :)'
         return text
 
 
